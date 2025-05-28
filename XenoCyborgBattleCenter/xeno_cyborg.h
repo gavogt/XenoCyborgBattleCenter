96f9ad5e829e1b7cd828a1c6ebecbb737c3969e7
Gabriel <gvogt@asu.edu>
2025-05-28 13:05:13 -0700

    Added an AlienCyborg struct to xeno_cyborg.h

--- XenoCyborgBattleCenter/xeno_cyborg.h
+++ XenoCyborgBattleCenter/xeno_cyborg.h
@@ -3,4 +3,14 @@
 
 typedef enum { SCOUT, HEAVY_GUNNER, ENGINEER, MEDIC, DRONE_OPERATOR, FIELD_COMMANDER, DEMOLITION, PSI_OPERATIVE } CyborgRole;
 
+typedef struct {
+	int id;                     // Unique identifier for the cyborg
+	char name[50];             // Name of the cyborg
+	CyborgRole role;           // Role of the cyborg
+	int health;                // Health points of the cyborg
+	int attack_power;          // Attack power of the cyborg
+	int defense_power;         // Defense power of the cyborg
+	int speed;                 // Speed of the cyborg
+} AlienCyborg;;
+
 #endif // !XENO_CYBORG_H
